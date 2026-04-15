f5d1c5d52e682866cb034e1c1a8e8ab8285cb71e
Brandhoej <akbr@cs.aau.dk>
2026-04-15 20:21:54 +0200

    doc(graphs): forward

--- src/graphs/forward.rs
+++ src/graphs/forward.rs
@@ -1,10 +1,13 @@
 use crate::graphs::graph::Directed;
 
+/// Successor view of a directed graph.
 pub trait Forward: Directed {
+    /// Iterator over successors as `(from, edge, to)`.
     type Successors<'a>: Iterator<Item = (Self::Vertex, Self::Edge, Self::Vertex)>
     where
         Self: 'a;
 
+    /// Returns all successors of `vertex`.
     fn successors(&self, vertex: Self::Vertex) -> Self::Successors<'_>;
 }
 
@@ -17,6 +20,7 @@ where
     where
         Self: 'a;
 
+    #[inline]
     fn successors(&self, vertex: Self::Vertex) -> Self::Successors<'_> {
         self.outgoing(vertex)
     }
