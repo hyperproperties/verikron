1ce2ccb424bfa02a73a753ac8c59374d3d7f6ed2
Brandhoej <akbr@cs.aau.dk>
2026-04-15 14:58:57 +0200

    doc(graphs): backward

--- src/graphs/backward.rs
+++ src/graphs/backward.rs
@@ -1,10 +1,19 @@
 use crate::graphs::graph::Directed;
 
+/// Backward exploration for directed graphs.
+///
+/// This is a thin convenience layer over [`Directed`] that exposes incoming
+/// edges under predecessor terminology.
 pub trait Backward: Directed {
+    /// Iterator over predecessor edges of a vertex.
+    ///
+    /// Each item is `(source, edge, destination)`, where `destination`
+    /// equals the queried vertex.
     type Predecessors<'a>: Iterator<Item = (Self::Vertex, Self::Edge, Self::Vertex)>
     where
         Self: 'a;
 
+    /// Returns all predecessor edges of `vertex`.
     fn predecessors(&self, vertex: Self::Vertex) -> Self::Predecessors<'_>;
 }
 
@@ -13,10 +22,11 @@ where
     T: Directed,
 {
     type Predecessors<'a>
-        = T::Ingoing<'a>
+        = <T as Directed>::Ingoing<'a>
     where
         Self: 'a;
 
+    #[inline]
     fn predecessors(&self, vertex: Self::Vertex) -> Self::Predecessors<'_> {
         self.ingoing(vertex)
     }
