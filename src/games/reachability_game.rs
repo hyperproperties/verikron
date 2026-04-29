89c21c94e93bafd10a1d305caaf69494b0ed13cf
Brandhoej <akbr@cs.aau.dk>
2026-04-29 17:30:05 +0200

    fmt(games)

--- src/games/reachability_game.rs
+++ src/games/reachability_game.rs
@@ -14,7 +14,6 @@ use crate::{
         strategy::SynthesisResult,
     },
     graphs::{
-        expansion::{BackwardExpansion, Expansion, ForwardExpansion},
         graph::FiniteDirected,
         structure::{FiniteEdges, FiniteVertices, Structure},
     },
